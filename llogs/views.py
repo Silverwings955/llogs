7cb7c51aa9a329613e84c2420223a0ca347a3c34
silverwings955 <ssrs_2318069234@163.com>
2021-05-16 17:37:57 +0800

    补交

--- llogs/views.py
+++ llogs/views.py
@@ -1,5 +1,6 @@
 # coding=UTF-8
 from django.shortcuts import render
+from .models import Topic
 
 
 # Create your views here.
@@ -7,3 +8,10 @@ def index(request):
     """学习笔记的主页"""
     # return HttpResponse('是的，你成功了！')
     return render(request, 'llogs/index.html')
+
+
+def topics(request):
+    """显示所有主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'llogs/topics.html', context)
